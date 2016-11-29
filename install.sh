dc2d3766b656cd793cdba1e74b258c142ffc74f6
Alexandre Estela <alexandre.estela@actility.com>
2016-11-29 19:15:07 +0100

    fixes

--- install.sh
+++ install.sh
@@ -2,27 +2,28 @@
 
 # tested with raspbian ISO 2016-09-23-raspbian-jessie.img
 # assumes this action was already performed before running this install.sh script:
-# cd /home/pi && git clone https://github.com/actility/demo-smartbuilding-device
+# cd /home/pi && git clone https://github.com/actility/demo-smartbuilding-device && chmod +x demo-smartbuilding-device/install.sh && ./demo-smartbuilding-device/install.sh
 
+echo '***************************************'
 echo '* Demo SmartBuilding Device installer *'
-echo 'Type "raspberry" when asked for root password (default).'
+echo '* Powered with LoRa by Actility       *'
+echo '***************************************'
 
 sudo apt-get update
 
 # sudo apt-get upgrade -y
 # sudo apt-get dist-upgrade –y
 
-setxkbmap fr
+echo 'setxkbmap fr' >> /home/pi/.profile
 
-sudo apt-get install -y node
+sudo apt-get install -y nodejs npm
 
 sudo sed -i '/README/alcd_rotate=2' /boot/config.txt
 
-sudo sed -i '/[SeatDefaults]/axserver-command=X -nocursor -s 0 dpms' /etc/lightdm/lightdm.conf
+sudo sed -i '/# Seat defaults/axserver-command=X -nocursor -s 0 dpms' /etc/lightdm/lightdm.conf
 
 echo '#!/bin/bash
-cd /home/pi/demo-smartbuilding-device
-node server.js' >> /home/pi/launcher
+cd /home/pi/demo-smartbuilding-device && node server.js' >> /home/pi/launcher
 
 mv /home/pi/.config/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/autostart.bak
 echo '@lxpanel --profile LXDE-pi
