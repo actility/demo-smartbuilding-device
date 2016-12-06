3d4a7755a3c968230835b6d20ab5803c5a792c38
Alexandre Estela <alexandre.estela@actility.com>
2016-12-06 12:25:38 +0100

    fixes

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo apt-get install -y nodejs
 
 sudo sed -i '/README/alcd_rotate=2' /boot/config.txt
 
-sudo sed -i '/# Seat defaults/axserver-command=X -nocursor -s 0 dpms' /etc/lightdm/lightdm.conf
+sudo sed -i '/# Seat defaults/axserver-command=X -nocursor -s 0 -dpms' /etc/lightdm/lightdm.conf
 
 echo '#!/bin/bash
 cd /home/pi/demo-smartbuilding-device && node server.js' >> /home/pi/launcher.sh
@@ -33,7 +33,7 @@ echo '@lxpanel --profile LXDE-pi
 @xscreensaver -no-splash
 @point-rpi
 @lxterminal -e "/home/pi/launcher.sh"
-@chromium-browser --start-fullscreen http://localhost:8080 ' >> /home/pi/.config/lxsession/LXDE-pi/autostart
+@chromium-browser --disable-infobars --disable-session-crashed-bubble --start-fullscreen http://localhost:8080 ' >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
 cd /home/pi/demo-smartbuilding-device && npm install serialport && npm install
 
